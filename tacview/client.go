d6b945b751ccf794854bd64317226261d5dd1542
Barinzaya <barinzaya@gmail.com>
2022-05-22 16:19:11 -0400

    Added a fallback to CRC32 if the CRC64 password isn't accepted.
    This is what the actual Tacview application does, because apparently the DCS exporter doesn't actually support the CRC64 hash that the documentation says the protocol uses.

--- tacview/client.go
+++ tacview/client.go
@@ -4,13 +4,28 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"hash/crc32"
 	"hash/crc64"
+	"io"
 	"math/bits"
 	"net"
 	"unicode/utf16"
 )
 
-func hashPassword(password string) uint64 {
+func hashPassword32(password string) string {
+	password_utf16 := utf16.Encode([]rune(password))
+
+	password_bytes := make([]byte, 2*len(password_utf16))
+	for i, r := range password_utf16 {
+		password_bytes[2*i+0] = byte(r >> 0)
+		password_bytes[2*i+1] = byte(r >> 8)
+	}
+
+	hash := crc32.ChecksumIEEE(password_bytes)
+	return fmt.Sprintf("%x", hash)
+}
+
+func hashPassword64(password string) string {
 	password_utf16 := utf16.Encode([]rune(password))
 
 	password_bytes := make([]byte, 2*len(password_utf16))
@@ -19,12 +34,22 @@ func hashPassword(password string) uint64 {
 		password_bytes[2*i+1] = bits.Reverse8(byte(r >> 8))
 	}
 
-	hash := crc64.Checksum(password_bytes, crc64.MakeTable(crc64.ECMA))
-	return bits.Reverse64(hash)
+	hash := bits.Reverse64(crc64.Checksum(password_bytes, crc64.MakeTable(crc64.ECMA)))
+	return fmt.Sprintf("%x", hash)
 }
 
 /// Creates a new Reader from a TacView Real Time server
 func NewRealTimeReader(connStr string, username string, password string) (*Reader, error) {
+	reader, err := newRealTimeReaderHash(connStr, username, password, hashPassword64)
+
+	if err == io.EOF {
+		reader, err = newRealTimeReaderHash(connStr, username, password, hashPassword32)
+	}
+
+	return reader, err
+}
+
+func newRealTimeReaderHash(connStr string, username string, password string, hasher func(string) string) (*Reader, error) {
 	conn, err := net.Dial("tcp", connStr)
 	if err != nil {
 		return nil, err
@@ -71,23 +96,15 @@ func NewRealTimeReader(connStr string, username string, password string) (*Reade
 	if err != nil {
 		return nil, err
 	}
-	_, err = conn.Write([]byte(fmt.Sprintf("Client %s\n", username)))
+	_, err = conn.Write([]byte(fmt.Sprintf("%s\n", username)))
 	if err != nil {
 		return nil, err
 	}
 
-	if password != "" {
-		hash := hashPassword(password)
-
-		_, err = conn.Write([]byte(fmt.Sprintf("%x\x00\n", hash)))
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		_, err = conn.Write([]byte("\x00\n"))
-		if err != nil {
-			return nil, err
-		}
+	hash := hashPassword64(password)
+	_, err = conn.Write([]byte(fmt.Sprintf("%x\x00", hash)))
+	if err != nil {
+		return nil, err
 	}
 
 	return NewReader(reader)
